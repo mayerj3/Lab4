d5a83cd8dfd04ab8ca2cad51a84639eb73842035
mayerj3 <mayerj3@mail.uc.edu>
2021-02-10 04:22:53 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+class Main {  
+  public static void main(String[] args) {
+    Random r = new Random();
+    System.out.println(r.nextInt(51));
+    System.out.println("Hello world!");
+  }
+}
