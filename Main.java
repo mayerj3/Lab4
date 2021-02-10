fcea5b35aad5c179613c202d52ecd19e2c6da2de
mayerj3 <mayerj3@mail.uc.edu>
2021-02-10 04:56:41 +0000

    added if, else if, and else and their respective system prints

--- Main.java
+++ Main.java
@@ -1,9 +1,44 @@
+/*
+Lab 4
+Partners: John Mayer and Anthony Hale
+Date: 2/9/2021
+*/
 import java.util.Random;
 class Main {  
   public static void main(String[] args) {
     Random r = new Random();
     int randomNum = r.nextInt(51);
     System.out.println("The Random number is:" + randomNum);
+
+    if ( randomNum <= 15) 
+    {
+      while(randomNum >= 0)
+      {
+	    System.out.println(randomNum);
+      randomNum--;
+      }
+    System.out.println("Ahoy Matey!");
+    }
+
+
+   else if(randomNum > 20 && randomNum < 42) 
+    {
+    while(randomNum >= 0)
+     {
+	    System.out.println(randomNum);
+      randomNum--;
+     }
+        System.out.println("Cannonball!");
+    }
+
     
+    else{ 
+      while(randomNum>=0)
+      {
+        System.out.println(randomNum);
+        randomNum--;
+      }
+      System.out.println("Look, a sea shanty!");
+    }
   }
 }
